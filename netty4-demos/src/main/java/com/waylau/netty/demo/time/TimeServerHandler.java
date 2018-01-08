e264e0b3dc2ebed571cefab6a6e714bc51721795
Neil <1599369235@qq.com>
2018-01-08 21:41:05 +0800

    或者，你可以使用简单的预定义监听器代码:
    f.addListener(ChannelFutureListener.CLOSE);

--- netty4-demos/src/main/java/com/waylau/netty/demo/time/TimeServerHandler.java
+++ netty4-demos/src/main/java/com/waylau/netty/demo/time/TimeServerHandler.java
@@ -7,25 +7,25 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class TimeServerHandler extends ChannelInboundHandlerAdapter {
-	
+
 	@Override
-    public void channelActive(final ChannelHandlerContext ctx) { // (1)
-        final ByteBuf time = ctx.alloc().buffer(4); // (2)
-        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+	public void channelActive(final ChannelHandlerContext ctx) { // (1)
+		final ByteBuf time = ctx.alloc().buffer(4); // (2)
+		time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
 
-        final ChannelFuture f = ctx.writeAndFlush(time); // (3)
-        f.addListener(new ChannelFutureListener() {
-            @Override
-            public void operationComplete(ChannelFuture future) {
-                assert f == future;
-                ctx.close();
-            }
-        }); // (4)
-    }
+		final ChannelFuture f = ctx.writeAndFlush(time); // (3)
+		f.addListener(new ChannelFutureListener() {
+			@Override
+			public void operationComplete(ChannelFuture future) {
+				assert f == future;
+				ctx.close();
+			}
+		}); // (4)或者，你可以使用简单的预定义监听器代码:f.addListener(ChannelFutureListener.CLOSE);
+	}
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        cause.printStackTrace();
-        ctx.close();
-    }
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
 }
